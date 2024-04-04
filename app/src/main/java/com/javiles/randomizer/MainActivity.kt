a9a77aff6cbf41aea40b9c5e1592fff14794539d
joseph <joseph_aviles@hotmail.com>
2024-04-04 19:18:02 -0400

    finished

--- app/src/main/java/com/javiles/randomizer/MainActivity.kt
+++ app/src/main/java/com/javiles/randomizer/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.javiles.randomizer
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val results = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener{
+            val userBound = seekBar.progress + 1
+            val result = Random.nextInt(userBound)
+            results.text = result.toString()
         }
     }
 }
